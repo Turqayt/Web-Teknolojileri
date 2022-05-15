e6367415b995beac7d0cf60eeb7d9eaedac8d955
Turqay <58841404+Turqayt@users.noreply.github.com>
2022-05-15 19:14:47 +0300

    Add files via upload

---
+++ g191210552/G191210552/form.php
@@ -0,0 +1,80 @@
+
+<!doctype html>
+<html lang="tr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <title>İletişim Veri</title>
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </head>
+  <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">
+                    <img src="images/user.png" alt="Turgay Teymurov" title="Turgay Teymurov">
+                <span class="text text-right hidden-xs"><p>Turgay Teymurov</p></span>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup"> 
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                          <a class="nav-link" href="index.html"><b>Anasayfa</b></a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="hakkimda.html"><b>Hakkımda</b></a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="ozgecmis.html"><b>Özgeçmiş</b></a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="sehrim.html"><b>Şehrim</b></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="takimimiz.html"><b>Takımımız</b></a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="login.php"><b>Login</b></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="iletisim.html"><b>İletişim</b></a>
+                        </li>
+                     </ul>
+                </div>
+                </div>
+              </nav>
+            </div>
+        </header>
+ 
+        <div class="ciktilar">
+        <?php 
+        $isim=$_POST["isim"];
+        $mail=$_POST["e-mail"];
+        $phone=$_POST["phone"];
+        $message=$_POST["message"];
+
+
+        print "İsim: $isim" ;
+        echo '<br>' ;
+        print "Mail: $mail" ;
+        echo '<br>' ;
+        print "Telefon: $phone" ;
+        echo '<br>' ;
+        print "Mesaj: $message" ;
+        echo '<br>' ;
+
+        ?>
+        </div>
+
+  </body>
+</html>
